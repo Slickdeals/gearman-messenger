7c1a4ee3bf264d4c97c51a5551c00aae774423ac
Ryan Rogers <ryan.rogers@slickdeals.net>
2022-06-14 16:16:02 -0700

    Added support for Symfony 6.x

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+$finder = PhpCsFixer\Finder::create()
+    ->ignoreDotFiles(false)
+    ->ignoreVCSIgnored(true)
+    ->in(__DIR__)
+;
+
+$config = new PhpCsFixer\Config();
+$config
+    ->setRiskyAllowed(false)
+    ->setRules([
+        '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+    ])
+    ->setFinder($finder)
+;
+
+return $config;
